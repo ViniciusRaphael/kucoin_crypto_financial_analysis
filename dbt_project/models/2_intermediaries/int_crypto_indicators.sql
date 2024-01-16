cf884e984daf6a55cd91703e0d67659bf7cbae96
Vinicius Raphael <vinicius.raphael92@gmail.com>
2024-01-16 01:55:36 -0300

    Updating column names

--- dbt_project/models/2_intermediaries/int_crypto_indicators.sql
+++ dbt_project/models/2_intermediaries/int_crypto_indicators.sql
@@ -9,13 +9,13 @@ WITH stg_crypto_hist_price AS (
 final AS (
     SELECT
         *,
-        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS ma7,
-        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS ma30,
-        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 99 PRECEDING AND CURRENT ROW) AS ma100,
-        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 199 PRECEDING AND CURRENT ROW) AS ma200,
-        SUM(CASE WHEN vl_close > vl_open THEN vl_close - vl_open ELSE 0 END) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW) AS gain,
-        SUM(CASE WHEN vl_open > vl_close THEN vl_open - vl_close ELSE NULL END) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW) AS loss,
-        (AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 11 PRECEDING AND CURRENT ROW) - AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW)) AS macd
+        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS vl_ma7,
+        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS vl_ma30,
+        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 99 PRECEDING AND CURRENT ROW) AS vl_ma100,
+        AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 199 PRECEDING AND CURRENT ROW) AS vl_ma200,
+        SUM(CASE WHEN vl_close > vl_open THEN vl_close - vl_open ELSE 0 END) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW) AS vl_gain,
+        SUM(CASE WHEN vl_open > vl_close THEN vl_open - vl_close ELSE NULL END) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW) AS vl_loss,
+        (AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 11 PRECEDING AND CURRENT ROW) - AVG(vl_close) OVER (PARTITION BY nm_symbol ORDER BY dt_date ROWS BETWEEN 23 PRECEDING AND CURRENT ROW)) AS vl_macd
     FROM stg_crypto_hist_price
 )
 
