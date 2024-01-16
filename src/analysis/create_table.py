912bff139830208b54eab4058d7aafe0ad4c4b84
Vinicius Raphael <vinicius.raphael92@gmail.com>
2024-01-16 01:47:35 -0300

    Updating sqlalchemy to drop table and dependencies

--- src/analysis/create_table.py
+++ src/analysis/create_table.py
@@ -2,12 +2,19 @@ import pandas as pd
 from sqlalchemy import create_engine
 from configparser import ConfigParser
 
+# Read configuration from 'config.ini' file
 config = ConfigParser()
 config.read('config.ini')
 
 def read_csv(file_path):
     """
     Read CSV file into a DataFrame.
+    
+    Parameters:
+    - file_path (str): Path to the CSV file.
+
+    Returns:
+    - pd.DataFrame: DataFrame containing the CSV data.
     """
     df = pd.read_csv(file_path)
     return df
@@ -15,6 +22,12 @@ def read_csv(file_path):
 def connect_to_database(connection_params):
     """
     Connect to the PostgreSQL database.
+    
+    Parameters:
+    - connection_params (dict): Dictionary containing database connection parameters.
+
+    Returns:
+    - sqlalchemy.engine.Engine: SQLAlchemy engine for database connection.
     """
     try:
         engine = create_engine(f"postgresql://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
@@ -27,12 +40,23 @@ def connect_to_database(connection_params):
 def load_data_into_database(df, engine, table_name, if_exists='replace'):
     """
     Load DataFrame data into the PostgreSQL database.
+    
+    Parameters:
+    - df (pd.DataFrame): DataFrame containing data to be loaded.
+    - engine (sqlalchemy.engine.Engine): SQLAlchemy engine for database connection.
+    - table_name (str): Name of the table to which data will be loaded.
+    - if_exists (str): Specifies behavior when the table already exists. Default is 'replace'.
+
+    Returns:
+    - None
     """
     try:
         df.to_sql(table_name, engine, if_exists=if_exists, index=False)
         print(f'DataFrame data loaded into {table_name} table')
     except Exception as e:
-        print(f'Error loading data into {table_name} table: {e}')
+        # Drop the table with CASCADE if an error occurs during loading
+        drop_cascade_sql = (f'DROP TABLE {table_name} CASCADE')
+        engine.execute(drop_cascade_sql)
 
 def main():
     # Configuration
